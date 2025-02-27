4373f1f5b8fbe1237798ce6063072c9b7d57afd9
Enrique Torreblanca <enriquetchat@gmail.com>
2025-02-26 16:22:30 -0800

    Update component.js

--- components/component.js
+++ components/component.js
@@ -81,7 +81,7 @@ const Component = () => {
         alignItems: "center",
         paddingTop: "10px",
         minHeight: "auto",
-        boxShadow: "0 0 20px rgb(255 255 255 / 80%)"
+        boxShadow: "0 0 30px rgb(0 0 0 / 100%)"
 
       }}
     >
